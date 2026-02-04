c0a07c33bfffb08f58cf48e905fb75fa3a94d57a
sithija-jayasinghe <harshanajayasinghe113@gmail.com>
2026-02-04 10:51:44 +0530

    feat: add options prop to Select component for dynamic option rendering

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cn } from '../utils';
 
-const Select = React.forwardRef(({ className, children, ...props }, ref) => {
+const Select = React.forwardRef(({ className, children, options, label, ...props }, ref) => {
   return (
     <div className="relative">
       <select
@@ -12,7 +12,11 @@ const Select = React.forwardRef(({ className, children, ...props }, ref) => {
         ref={ref}
         {...props}
       >
-        {children}
+        {options ? options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        )) : children}
       </select>
       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
