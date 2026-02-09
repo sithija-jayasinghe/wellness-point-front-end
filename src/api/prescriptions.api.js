442849926204f20ab376c64d432b05d6a9e4c098
Asinsala2001 <112684739+Asinsala2001@users.noreply.github.com>
2026-02-09 17:04:56 +0530

    feat: update prescription API endpoints to include '/api' prefix

--- src/api/prescriptions.api.js
+++ src/api/prescriptions.api.js
@@ -1,26 +1,26 @@
 import http from './http';
 
 export const getAllPrescriptions = async () => {
-    const response = await http.get('/prescriptions/get-all');
+    const response = await http.get('/api/prescriptions');
     return response.data;
 };
 
 export const getPrescriptionById = async (id) => {
-    const response = await http.get(`/prescriptions/${id}`);
+    const response = await http.get(`/api/prescriptions/${id}`);
     return response.data;
 };
 
 export const createPrescription = async (data) => {
-    const response = await http.post('/prescriptions/add', data);
+    const response = await http.post('/api/prescriptions', data);
     return response.data;
 };
 
 export const updatePrescription = async (id, data) => {
-    const response = await http.put(`/prescriptions/update/${id}`, data);
+    const response = await http.put(`/api/prescriptions/${id}`, data);
     return response.data;
 };
 
 export const deletePrescription = async (id) => {
-    const response = await http.delete(`/prescriptions/delete/${id}`);
+    const response = await http.delete(`/api/prescriptions/${id}`);
     return response.data;
 };
